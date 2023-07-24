d40e7c02f490822eaf3f6c48254484830dbd6999
niccoloruberti <nikiruberti@gmail.com>
2023-07-24 15:44:26 +0200

    aggiunto contenuto main tramite yield

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,41 +1,19 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-</head>
-
-<body>
-    <!-- header -->
-    @include ('partials.header')
-    <!-- jumbotron -->
-    <div class="jumbotron">
-        <button>current series</button>
-    </div>
-    <!-- main -->
-    <main class="bg-dark py-5">
-        <div class="container">
-            <div class="row">
-                @foreach($comicsThumb as $comic)
-                    <div class="col-2 my-3">
-                        <div class="card-content h-100 w-100 d-flex flex-column align-items-center">
-                            <img src="{{ $comic['thumb'] }}">
-                            <h3 class=" mt-2 text-center text-white text-uppercase w-100"> {{ $comic['title'] }}</h3>
-                        </div>
+@section('content')
+    <div class="container">
+        <div class="row">
+            @foreach($comicsThumb as $comic)
+                <div class="col-2 my-3">
+                    <div class="card-content h-100 w-100 d-flex flex-column align-items-center">
+                        <img src="{{ $comic['thumb'] }}">
+                        <h3 class=" mt-2 text-center text-white text-uppercase w-100"> {{ $comic['title'] }}</h3>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
-        <div class="text-center">
-            <button>load more</button>
-        </div>
-    </main>
-    <!-- footer -->
-    @include ('partials.footer')
-</body>
-
-</html>
+    </div>
+    <div class="text-center">
+        <button>load more</button>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <!-- header -->
+    @include ('partials.header')
+    <!-- jumbotron -->
+    <div class="jumbotron">
+        <button>current series</button>
+    </div>
+    <!-- main -->
+    <main class="bg-dark py-5">
+        @yield('content')
+    </main>
+    <!-- footer -->
+    @include ('partials.footer')
+</body>
+
+</html>
